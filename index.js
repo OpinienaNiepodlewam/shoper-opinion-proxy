afc36d25af29005929280646bf0e08ff9018c52d
OpinienaNiepodlewam <niepodlewam.reklamy@gmail.com>
2025-07-14 12:20:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', 'https://pl.sklep88test2.shoperapp.pl');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    return res.status(200).end(); // CORS preflight response
+    return res.status(200).end(); // Preflight OK
   }
 
   if (req.method !== 'POST') {
@@ -14,13 +14,11 @@ export default async function handler(req, res) {
   try {
     const { productId, name, rating, comment } = req.body;
 
-    // Możesz dodać logikę np. wysyłki do Google Sheets tutaj
-
     console.log('DANE OTRZYMANE:', { productId, name, rating, comment });
 
     return res.status(200).json({ message: 'Opinia odebrana!' });
   } catch (err) {
-    console.error('Błąd po stronie API:', err);
+    console.error('Błąd:', err);
     return res.status(500).json({ error: 'Błąd serwera' });
   }
 }
